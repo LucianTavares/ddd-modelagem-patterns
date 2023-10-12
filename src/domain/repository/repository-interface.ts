13438abe0aa29b756f47b86234e593381f0cd061
Vinicius Italo da Cruz <viniciuscruz@frwk.com.br>
2023-10-12 10:49:02 -0300

    refactor: update in repository interface

--- src/domain/repository/repository-interface.ts
+++ src/domain/repository/repository-interface.ts
@@ -1,7 +1,8 @@
 export interface IRepository<T> {
     findAll(): Promise<T[]>;
-    update(id: string): Promise<T>;
+    update(entity: T): Promise<void>;
+    findById(id: string): Promise<T>;
     create(entity: T): Promise<void>;
     update(entity: T): Promise<void>;
-    delate(id: string): Promise<void>;
+    delete(id: string): Promise<void>;
 }
